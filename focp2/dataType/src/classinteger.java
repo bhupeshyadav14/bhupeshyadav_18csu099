19a84af52007ee575104c0201840682387c370b3
bhupeshyadav14 <46473473+bhupeshyadav14@users.noreply.github.com>
2019-01-11 11:09:06 +0530

    Add files via upload

---
+++ focp2/dataType/src/classinteger.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class classinteger 
+{
+    public static void main(String[] args)
+    {
+    	Scanner input = new Scanner(System.in);
+          int a=3,b=4,add,sub,div,mod,mul;
+          add=a+b;
+          sub=a-b;
+          div=a/b;
+          mod=a%b;
+          mul=a*b;
+          System.out.println(add);
+          System.out.println(sub);
+          System.out.println(div);
+          System.out.println(mod);
+          System.out.println(mul);
+      }
+  }
+  
